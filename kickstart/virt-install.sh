2a64ec87c9fd80564f7d6f52337a4aebb1ea414f
Luke Short <ekultails@gmail.com>
2023-03-03 14:47:11 -0500

    [kickstart] virt-install with UEFI
    We need to test and ensure that UEFI works instead of legacy boot.

--- kickstart/virt-install.sh
+++ kickstart/virt-install.sh
@@ -5,6 +5,7 @@ sudo virt-install \
   --virt-type kvm --graphics spice --video vga\
   --name playtron-dev-os \
   --os-variant fedora37 \
+  --boot uefi \
   --vcpus 4 --ram 16384 \
   --disk=/var/lib/libvirt/images/playtron-dev-os.img,bus=virtio,format=raw,size=64 \
   --location=https://mirror.rackspace.com/fedora/releases/37/Everything/x86_64/os/ \
