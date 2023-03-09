77003f3f4a581c65b3e9d4a7d48a74e178962175
Luke Short <ekultails@gmail.com>
2023-03-09 13:15:16 -0700

    [kickstart] Do not reboot
    When the virtual machine is done being built, do not boot. Booting
    will cause various files and settings to be automatically
    generated.

--- kickstart/virt-install.sh
+++ kickstart/virt-install.sh
@@ -10,4 +10,5 @@ sudo virt-install \
   --disk=/var/lib/libvirt/images/playtron-dev-os.img,bus=virtio,format=raw,size=64 \
   --location=https://mirror.rackspace.com/fedora/releases/37/Everything/x86_64/os/ \
   --initrd-inject ./playtron-dev-os_kickstart.cfg \
-  --extra-args="inst.ks=file:/playtron-dev-os_kickstart.cfg net.ifnames=0 biosdevname=0"
+  --extra-args="inst.ks=file:/playtron-dev-os_kickstart.cfg net.ifnames=0 biosdevname=0" \
+  --noreboot
