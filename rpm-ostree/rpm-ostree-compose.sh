26561fe65baa4bbc7a87c587f30a6e836707bdbc
Luke Short <ekultails@gmail.com>
2023-03-15 10:50:35 -0600

    [kickstart] Generate a portable initramfs
    Resolves #2

--- rpm-ostree/rpm-ostree-compose.sh
+++ rpm-ostree/rpm-ostree-compose.sh
@@ -3,5 +3,5 @@
 WORKDIR=/tmp
 mkdir -p ${WORKDIR}/cache ${WORKDIR}/repo
 sudo ostree --repo=${WORKDIR}/repo init --mode=archive-z2
-sudo rpm-ostree compose tree --unified-core --cachedir=${WORKDIR}/cache --repo=${WORKDIR}/repo fedora-silverblue.yaml
+sudo rpm-ostree compose tree --unified-core --cachedir=${WORKDIR}/cache --repo=${WORKDIR}/repo playtron-dev-os.yaml
 sudo ostree summary --repo=${WORKDIR}/repo --update
