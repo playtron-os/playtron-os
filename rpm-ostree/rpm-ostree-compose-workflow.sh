d86116e98f64909b8b8c32aa6c037e14aae7a659
Alesh Slovak <aleshslovak@gmail.com>
2023-10-11 17:08:12 -0400

    install rpm-ostree

---
+++ rpm-ostree/rpm-ostree-compose-workflow.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+dnf install rpm-ostree
+./rpm-ostree-compose.sh
--- rpm-ostree/rpm-ostree-compose.sh
+++ rpm-ostree/rpm-ostree-compose.sh
@@ -2,6 +2,6 @@
 
 WORKDIR="/root/tmp"
 mkdir -p ${WORKDIR}/cache ${WORKDIR}/repo
-sudo ostree --repo=${WORKDIR}/repo init --mode=archive-z2
-sudo rpm-ostree compose tree --unified-core --cachedir=${WORKDIR}/cache --repo=${WORKDIR}/repo playtron-dev-os.yaml
-sudo ostree summary --repo=${WORKDIR}/repo --update
+ostree --repo=${WORKDIR}/repo init --mode=archive-z2
+rpm-ostree compose tree --unified-core --cachedir=${WORKDIR}/cache --repo=${WORKDIR}/repo playtron-dev-os.yaml
+ostree summary --repo=${WORKDIR}/repo --update
