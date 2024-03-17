69490cb6344aefd9d346d6c5064915e2664bf202
Luke Short <ekultails@gmail.com>
2024-03-17 14:42:41 -0600

    [rpm-ostree][kickstart] Update to Fedora 40
    Rebase upstream files from Fedora 39 Silverblue. Reference git
    commit: 5de23ba3.
    
    https://pagure.io/workstation-ostree-config/tree/f40

--- kickstart/virt-install.sh
+++ kickstart/virt-install.sh
@@ -4,11 +4,11 @@ sudo virt-install \
   --connect qemu:///system \
   --virt-type kvm --graphics spice --video vga\
   --name playtron-os \
-  --os-variant fedora39 \
+  --os-variant fedora40 \
   --boot uefi \
   --vcpus 4 --ram 16384 \
   --disk=/var/lib/libvirt/images/playtron-os.img,bus=virtio,format=raw,size=8 \
-  --location=https://mirror.rackspace.com/fedora/releases/39/Everything/x86_64/os/ \
+  --location=https://mirror.rackspace.com/fedora/development/40/Everything/x86_64/os/ \
   --initrd-inject ./playtron-os_kickstart.cfg \
   --extra-args="inst.ks=file:/playtron-os_kickstart.cfg net.ifnames=0 biosdevname=0" \
   --noreboot
