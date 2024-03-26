4ba44b7c369d1226ee7e925ce68dfc5a2e90bb96
Luke Short <ekultails@gmail.com>
2024-03-26 12:33:37 -0600

    [kickstart] Use 'fedora-unknown'
    as a generic template to create Fedora virtual machines from. We
    override the defaults anyways so this does not need to be a
    specific Fedora version.
    
    This is one less thing we need to worry about when upgrading to
    newer Fedora versions.

--- kickstart/virt-install.sh
+++ kickstart/virt-install.sh
@@ -5,7 +5,7 @@ sudo virt-install \
   --connect qemu:///system \
   --virt-type kvm --graphics spice --video vga\
   --name playtron-os \
-  --os-variant fedora39 \
+  --os-variant fedora-unknown \
   --boot uefi \
   --vcpus 4 --ram 16384 \
   --disk=/var/lib/libvirt/images/playtron-os.img,bus=virtio,format=raw,size=8 \
