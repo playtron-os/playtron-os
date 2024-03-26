6cf8ecb7e034b16a13d22e5b2b25e2addd955bc1
Luke Short <ekultails@gmail.com>
2024-03-25 18:14:01 -0600

    [kickstart] Use 'fedora39' variant
    as 'fedora40' is not a valid variant for 'virt-install' yet.

--- kickstart/virt-install.sh
+++ kickstart/virt-install.sh
@@ -4,7 +4,7 @@ sudo virt-install \
   --connect qemu:///system \
   --virt-type kvm --graphics spice --video vga\
   --name playtron-os \
-  --os-variant fedora40 \
+  --os-variant fedora39 \
   --boot uefi \
   --vcpus 4 --ram 16384 \
   --disk=/var/lib/libvirt/images/playtron-os.img,bus=virtio,format=raw,size=8 \
