8e4ebd8e943997a8b06c3842b8488a7229b4ac8b
Luke Short <ekultails@gmail.com>
2024-11-01 16:38:19 -0600

    Revert "[rpm-ostree][kickstart] Use Fedora 41 stable"
    This reverts commit f106ae4aabb396f769586d3c3e206a7c553e98a2.
    
    Our Alpha 2 launch has been undelayed so we are reverting back to
    Fedora 41 beta which has been more thoroughly tested. ASUS ROG
    Ally controlller support was also regressed when the Linux kernel
    was bumped from 6.10 to 6.11.

--- kickstart/virt-install.sh
+++ kickstart/virt-install.sh
@@ -8,7 +8,7 @@ sudo virt-install \
   --boot uefi \
   --vcpus 4 --ram 16384 \
   --disk=/var/lib/libvirt/images/playtron-os.img,bus=virtio,format=raw,size=9 \
-  --location=https://mirror.rackspace.com/fedora/releases/41/Everything/x86_64/os/ \
+  --location=https://mirror.rackspace.com/fedora/releases/40/Everything/x86_64/os/ \
   --initrd-inject ./playtron-os_kickstart.cfg \
   --extra-args="inst.ks=file:/playtron-os_kickstart.cfg net.ifnames=0 biosdevname=0" \
   --noreboot
